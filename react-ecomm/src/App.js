0017a19fab6610eed140444b70f3a1195f002519
deepakkumar9470 <dk397787@gmail.com>
2022-08-08 10:01:50 +0530

    second

--- react-ecomm/src/App.js
+++ react-ecomm/src/App.js
@@ -2,55 +2,22 @@ import React from 'react'
 import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './components/Home';
-import Cart from './components/Cart';
-import ProductDetails from './components/ProductDetails';
-import AdminDashboard from './admin/AdminDashboard';
-import AddProduct from './admin/AddProduct';
+
 import MyLogin from './pages/MyLogin';
 import MySignUp from './pages/MySignUp';
-import Navigation from './components/Navigation';
-import {useSelector } from 'react-redux';
-import ProductPage from './pages/ProductPage';
-import CategoryPage from './pages/CategoryPage';
-import OrdersPage from './pages/OrdersPage';
-import EditProductPage from './admin/EditProductPage';
-import ScrollToTop from './components/ScrollToTop';
+
+
 function App() {
-  const {user} = useSelector((state) => state);
+ 
   return (
     <div className="App">
       <BrowserRouter>
       <ScrollToTop/>
       <Navigation/>
          <Routes>
-            <Route index element={<Home/>}/>
-            <Route path="*" element={<Home/>}/>
-              {!user && <>
+                <Route path="/" exact element={<Home/>}/>            
                 <Route path='/login' element={<MyLogin/>}/>
-                  <Route path='/signup' element={<MySignUp/>}/>
-              </>}
-
-              {user && (
-                <>
-                   <Route path='/cart' element={<Cart/>}/>
-                </>
-              )
-              }
-
-            {user && user.isAdmin && (
-              <>
-                 <Route path='admin' element={<AdminDashboard/>}/>
-                 <Route path='/product/:id/edit' element={<EditProductPage/>}/>
-              </>
-              )
-              }
-            <Route path='/order' element={<OrdersPage/>}/>
-           
-            <Route path='/product/:id' element={<ProductPage/>}/>
-            <Route path='/category/:category' element={<CategoryPage/>}/>
-            
-            <Route path='/new-product' element={<AddProduct/>}/>
-            
+                <Route path='/signup' element={<MySignUp/>}/>
          </Routes>
       </BrowserRouter>
        
--- react-ecomm/src/index.js
+++ react-ecomm/src/index.js
@@ -6,22 +6,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "react-bootstrap/dist/react-bootstrap.min.js";
 import 'react-alice-carousel/lib/alice-carousel.css';
 
-import store from "./redux/store";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import persistStore from "redux-persist/es/persistStore";
 
-// store to persit
-const persistedStore = persistStore(store);
 
 ReactDOM.render(
-  <Provider store={store}>
-  <PersistGate loading={<div>Loading..</div>} persistor={persistedStore}>
-    <App />
-  </PersistGate>
-</Provider>
-   
-    ,
+    <App />,
 document.getElementById('root'));
 
 
